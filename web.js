5a92e31f9d378fb95816d1df736fe9f3b9fd6f24
Travis Meinders <tmeinders@gmail.com>
2013-06-28 01:17:50 -0400

    removing some unused code

--- web.js
+++ web.js
@@ -6,24 +6,7 @@ var redis = require("redis-url").connect(process.env.MYREDIS_URL);
 
 var app = express();
 
-app.get('*', function(request, response, next) {
-  
-  console.log('* is executing');
-  if (request.headers.host === 'www.meinde.rs')
-  {
-    response.redirect(302,'http://www.meinders.com/test2.html');
-    response.end();
-    console.log('redirecting...');
-  }
-  else
-  {
-    next();
-  }
-});
-
 app.get('/', function(request, response) {
-  console.log('host: ' + request.headers.host);
-  console.log('/ is executing');
   fs.readFile('./index.html', function(error, content) {
     if (error) {
       response.writeHead(500);
@@ -34,15 +17,16 @@ app.get('/', function(request, response) {
       response.end(content, 'utf-8');
     }
   });
-
-
-  //response.send('Hello World2!');
 });
 
-
-app.get('/test2.html', function(request, response) {
-  response.send('you are using the shortener 2');
+app.get('*', function(request, response, next) {
+  
+  response.redirect(302,'http://www.meinders.com/test2.html');
+  response.end();
+  console.log('redirecting...');
+    next();
 });
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
