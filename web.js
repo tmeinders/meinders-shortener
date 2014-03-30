3c0832bab046be123a128ff72445a5cf744e963f
Travis Meinders <tmeinders@gmail.com>
2014-03-30 18:29:57 -0400

    tying in redis

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var url = require('url');
 
-
-var redis = require("redis-url").connect(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+	var redis = require("redis").createClient();
+}
 
 var app = express();
+app.use(express.logger());
 
 app.get('/shorten', function(request, response) {
   fs.readFile('./shorten.html', function(error, content) {
@@ -19,12 +26,26 @@ app.get('/shorten', function(request, response) {
   });
 });
 
-app.get('*', function(request, response, next) {
-  
-  response.redirect(302,'http://www.meinders.com/test2.html');
+
+app.get('/shortenaction', function(request, response, next) {
+  var url_parts = url.parse(request.url, true);
+  console.log('shortenaction...');
+  redis.set(url_parts.query['path'],url_parts.query['url'],redis.print);
+  response.send("hi");
   response.end();
-  console.log('redirecting...');
-    //next();
+});
+
+
+app.get('*', function(request, response, next) {
+  var path = url.parse(request.url).pathname; 
+  console.log('path: ' + path);
+  redis.get(path, 
+  	function(err,reply){ 
+	  response.redirect(reply);
+	  response.end();
+      console.log('redirecting...');
+    }
+  );
 });
 
 
